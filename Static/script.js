c5ad4081c0dbcfe3630dcda7368c124d52b6eeb8
ziyal <wintermist3@gmail.com>
2017-07-18 10:28:48 -0700

    little bit of everything

---
+++ Static/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    $(".email").hover(
+        function () {
+            $(this).hide();
+            $(".email-overlay").show();
+        }, function () {
+            $(this).show();
+            $(".email-overlay").hide();
+    });
+
+    $(".github").hover(
+        function () {
+            $(this).hide();
+            $(".github-overlay").show();
+        }, function () {
+            $(this).show();
+            $(".github-overlay").hide();
+    });
+
+    $(".linkedin").hover(
+        function () {
+            $(this).hide();
+            $(".linkedin-overlay").show();
+        }, function () {
+            $(this).show();
+            $(".linkedin-overlay").hide();
+    });    
+
+});
+
+
+    // $(".email")
+    // .mouseenter(function() {
+    //     $(this).hide();
+    //     $(".email-overlay").show();
+    // })
+    // .mouseleave(function() {
+    //     $(this).show();
+    //     $(".email-overlay").hide();
+    // })
